86f1d5e749682665c9b0c1c79d7bcc5f31ccb96f
krishna kotha <kckotha@gmail.com>
2019-02-27 12:38:14 -0800

    updated v2.py file

--- v2.py
+++ v2.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2018  Krishna Kotha <krkotha@cisco.com>
+# All rights reserved.
+
+# import the requests library
+
 import sys
 import time
 import datetime
@@ -20,6 +27,7 @@ class NetworkDevice:
     def telnetToSwitch(self, switch):
         str_return_val = " "
         try:
+            #creates a telnet connection to the switch
             tn = telnetlib.Telnet(switch['HOST'], switch['port'], timeout=1)
             time.sleep(2)
             tn.write("\r")
@@ -27,24 +35,36 @@ class NetworkDevice:
 
             access_verify = '\r\n\r\nUser Access Verification\r\n\r\nUsername: '
 
+            # if switch prompts username then enter the username if not skip
             if tn.read_until("Username: ", 3) == access_verify:
                 tn.write(switch['user'] + "\r")
                 tn.read_until("Password: ", 3) == "Password"
                 tn.write(switch['password'] + "\r")
 
+            # typing in the enable secret password
             tn.write('enable' + "\r")
             time.sleep(2)
             tn.write(switch['enable_password'] + '\r')
             time.sleep(2)
+
+            # for the IOS XE device to get rid of the '---More---'
             tn.write("terminal length 0" + "\r")
             time.sleep(2)
+
+            # execute the command from the command file
             tn.write(commands.exec_command1 + "\r")
             time.sleep(4)
+
+            # exit out of the switch
             tn.write("exit\r")
             time.sleep(3)
+
+            #read the whatever output it displayed to the main function
             str_return_val = tn.read_very_eager()
             time.sleep(3)
             return str_return_val
+
+        #if any of the device is not reachable, it skips that device and continue on other devices
         except Exception:
             pass
             print ("\n\nThis switch is not reachable: %s %s \n" % (switch['HOST'], switch['port']))
