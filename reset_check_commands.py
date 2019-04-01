fa579cfdbc7d01e52a68fcdbf03f1af6779f4b11
larissaoverbey <48573710+larissaoverbey@users.noreply.github.com>
2019-04-01 16:41:54 -0700

    Add files via upload
    This will validate if AAA, CTS, VRF, LISP or BGP is existing on the device.  You can modify this based on what existing config you need to check.

---
+++ reset_check_commands.py
@@ -0,0 +1,9 @@
+#change  whatever command you want to execute in this file, for example listed here is dir | inc underlay
+exec_command1 = 'show run vrf'
+exec_command2 = 'show run | sec aaa'
+exec_command3 = 'show run | sec bgp'
+exec_command4 = 'show run | sec lisp'
+exec_command5 = 'show run | sec cts'
+
+
+
